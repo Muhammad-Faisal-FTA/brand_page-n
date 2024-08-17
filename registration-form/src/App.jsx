18434a48fe80345b53e2d590de41bb7a8968da96
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2024-08-17 16:43:06 -0700

    setup

---
+++ registration-form/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <div>
+    <h1 className='bg-red-700'>Registration</h1>
+   </div>
+   </>
+  );
+}
+
+export default App;
