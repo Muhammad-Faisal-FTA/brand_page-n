b5343a9b040b1c3cbd3af74b26a0c3ce7515cc39
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2024-08-26 21:06:53 -0700

    React + Tailwind set up coplete

---
+++ contect-page/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="text-center bg-blue-500 p-4 text-white">
+  Hello, Tailwind CSS!
+</div>
+
+    </div>
+  );
+}
+
+export default App;
