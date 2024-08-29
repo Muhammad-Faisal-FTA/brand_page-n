8706fcf9596e4b8043e4933aca1bfaabb0ded12f
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2024-08-28 21:23:16 -0700

    Removed deleted files from the index

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,21 @@
-
 import './App.css';
-import Arr from './arryinput'
+import { useState } from 'react';
+import { IoEyeOutline } from "react-icons/io5";
+import { GoEyeClosed } from "react-icons/go";
+
 
 function App() {
+  let [status , setStatus] =useState(false)
   return (
-    <div className="App">
-      <Arr/>
-    </div>
+    <>
+     <div className='flex'>
+     {
+      (status)?
+      <p>Welcome !</p> : '* * * * * *         \u00A0\u00A0\u00A0'
+     }
+      <button onClick={() => setStatus(!status)}> {(status)? <GoEyeClosed /> : <IoEyeOutline />} </button>
+     </div>
+    </>
   );
 }
 
